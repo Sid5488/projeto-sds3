7493f398f162a576934356f5da341510a59e9015
Sid5488 <guilhermenoobsaibot@gmail.com>
2021-05-03 23:55:38 -0300

    Graphics

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from 'components/Navbar';
 import Footer from 'components/Footer';
 import DataTable from 'components/DataTable';
+import BarChart from 'components/BarChart';
 
 const App = () => {
   return (
@@ -8,8 +9,26 @@ const App = () => {
       <Navbar />
       
       <div className="container">
-        <h1 className="text-primary">Hello World!</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas!</h1>
         
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas vendas</h5>
+
+            <BarChart />
+          </div>
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas vendas</h5>
+
+            <BarChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas vendas</h2>
+        </div>
+
         <DataTable />
       </div>
       
